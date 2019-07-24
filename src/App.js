5f569d9573011abb52e9ff4ffc1c9c8c283bdf74
liamst19 <50284753+liamst19@users.noreply.github.com>
2019-07-24 00:26:30 -0700

    Page title added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Section1 from './component.section1.js';
 
 function App() {
 
+  // Hook ------------------------------------
+  useEffect(() => {
+    document.title = 'Scroll Demo';
+  });
+
+  // Rendering more than one Section1 elements
   const items = [];
   for(let i = 0; i < 6; i++){
-    items.push(<Section1 />);
+    items.push(<Section1 key={ 'Section_' + i} />);
   };
 
   return (

b6f293ef447a19f8a12b0fbf08c70c1b6507e93b
liamst19 <50284753+liamst19@users.noreply.github.com>
2019-07-25 16:16:50 -0700

    edited url in code

--- src/component.JapaneseText.js
+++ src/component.JapaneseText.js
@@ -24,7 +24,7 @@ function JapaneseText(props) {
     return paragraphs;
   };
 
-  fetch(process.env.PUBLIC_URL + props.url)
+  fetch(process.env.PUBLIC_URL + '/' + props.url)
     .then(response => {
       return response.json();
     })

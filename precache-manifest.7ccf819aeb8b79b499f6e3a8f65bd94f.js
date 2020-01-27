e9cfa7207d2fb41454c8a99b7841d3df106806ff
liamst19 <50284753+liamst19@users.noreply.github.com>
2020-01-27 21:00:36 +0900

    Updates

---
+++ precache-manifest.7ccf819aeb8b79b499f6e3a8f65bd94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caba0334429836113a2d14fa40dcdacc",
+    "url": "/react-scroll-demo/index.html"
+  },
+  {
+    "revision": "d852840b61cfc7d0fb27",
+    "url": "/react-scroll-demo/static/css/main.5bdd95df.chunk.css"
+  },
+  {
+    "revision": "f05b472fa99463a90256",
+    "url": "/react-scroll-demo/static/js/2.897d9790.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-scroll-demo/static/js/2.897d9790.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d852840b61cfc7d0fb27",
+    "url": "/react-scroll-demo/static/js/main.dbc0b80e.chunk.js"
+  },
+  {
+    "revision": "6eb7163b0ffa8e573203",
+    "url": "/react-scroll-demo/static/js/runtime-main.ad4c78ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-scroll-demo/precache-manifest.3b6788d819f0699274a75f9487789d84.js"
+  "/react-scroll-demo/precache-manifest.7ccf819aeb8b79b499f6e3a8f65bd94f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-scroll-demo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
